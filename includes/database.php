507b727feaff6b1a969f898c877908d926ee462c
Esteban Oller <estebanoller83@gmail.com>
2022-10-30 20:58:55 -0300

    Modificaciones

--- includes/database.php
+++ includes/database.php
@@ -2,15 +2,15 @@
 
 $db = mysqli_connect(
     $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'], // ?? por si no tiene contraseña
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASS'], 
     $_ENV['DB_BD']
 );
 
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "error de depuración: " . mysqli_connect_error(); //Error xd
+    echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
-}
+} 
